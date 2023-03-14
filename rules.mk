8bb39bf6d5d051057ea8e1d6cb22966e68be06e4
Ergotu <lambrechtsjordi@hotmail.be>
2023-03-14 21:18:35 +0100

    test

--- rules.mk
+++ rules.mk
@@ -1,2 +1,21 @@
-CONVERT_TO = promicro_rp2040
-SRC += source.c
+# Disable unused features
+#LEADER_ENABLE = no
+#COMMAND_ENABLE = no
+#TERMINAL_ENABLE = no
+#KEY_LOCK_ENABLE = no
+#SLEEP_LED_ENABLE = no
+#VELOCIKEY_ENABLE = no
+MAGIC_ENABLE = no
+CONSOLE_ENABLE = no
+UNICODE_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+# Common features
+COMBO_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+BOOTMAGIC_ENABLE = yes
+
+VPATH += $(USER_PATH)/oled $(USER_PATH)/rgb $(USER_PATH)/features
+OPT_DEFS += -DCAPS_UNLOCK -DINIT_EE_HANDS_$(shell echo ${SPLIT}|tr a-z A-Z)
+SRC += ergotu.c
