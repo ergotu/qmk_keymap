29ad692e1d54b0c5bc1bfa0306c9ade2d249aee1
Ergotu <lambrechtsjordi@hotmail.be>
2023-03-16 15:56:11 +0100

    ree

--- rules.mk
+++ rules.mk
@@ -1,15 +1,1 @@
-ENCODER_ENABLE = yes
-
-VPATH += $(USER_PATH)/oled 
 SRC += ergotu.c
-
-# OLED
-ifeq ($(strip $(KEYBOARD)), $(filter $(KEYBOARD), splitkb/aurora/lily58/rev1))
-	OLED_ENABLE = yes
-	ifneq ($(strip $(OLED)),)
-		OPT_DEFS += -D${OLED}
-		SRC += oled-icons.c oled-luna.c
-	else
-		SRC += oled-icons.c oled-bongocat.c
-	endif
-endif
