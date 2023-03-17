c1ad35eea5b1f5a3c15f6e3931f5a3963e261bb3
Jordi Lambrechts <jordi@intigriti.com>
2023-03-17 12:03:49 +0100

    add conditional

--- rules.mk
+++ rules.mk
@@ -1,4 +1,15 @@
 VPATH += $(USER_PATH)/oled
 
 SRC += ergotu.c
-SRC += oled-icons.c oled-bongocat.c
+
+
+# OLED
+ifeq ($(strip $(KEYBOARD)), $(filter $(KEYBOARD), splitkb/aurora/lily58/rev1))
+	OLED_ENABLE = yes
+	ifneq ($(strip $(OLED)),)
+		OPT_DEFS += -D${OLED}
+		SRC += oled-icons.c oled-luna.c
+	else
+		SRC += oled-icons.c oled-bongocat.c
+	endif
+endif
