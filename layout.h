00da1e25eca3079f1ee441310e6db65d0b2f0431
Ergotu <lambrechtsjordi@hotmail.be>
2023-03-19 12:35:34 +0100

    hrm

--- layout.h
+++ layout.h
@@ -18,16 +18,8 @@
 #define TH(k1,k2,k3,k4) LT(0,k1),LT(0,k2),LT(0,k3),LT(0,k4)
 
 // Home row mod-tap macros
-#define HM_A	LSFT_T(KC_A)
-#define HM_S	LALT_T(KC_S)
-#define HM_D	LCTL_T(KC_D)
-#define HM_F	LGUI_T(KC_F)
-#define HM_J	LGUI_T(KC_J)
-#define HM_K	LCTL_T(KC_K)
-#define HM_L	LALT_T(KC_L)
-#define HM_QUOT	LSFT_T(KC_QUOT)
-#define HRML(k1,k2,k3,k4) LSFT_T(k1),LALT_T(k2),LCTL_T(k3),LGUI_T(k4)
-#define HRMR(k1,k2,k3,k4) LGUI_T(k1),LCTL_T(k2),LALT_T(k3),LSFT_T(k4)
+#define HRML(k1,k2,k3,k4) LGUI_T(k1),LALT_T(k2),LCTL_T(k3),LSFT_T(k4)
+#define HRMR(k1,k2,k3,k4) RSFT_T(k1),RCTL_T(k2),LALT_T(k3),RGUI_T(k4)
 
 // Navigation shortcuts
 #define SA_UP S(A(KC_UP))
