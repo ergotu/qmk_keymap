053ecc911d975960b1d219f2e8c170f2735f5522
Ergotu <lambrechtsjordi@hotmail.be>
2023-03-19 10:24:26 +0100

    add config

--- config.h
+++ config.h
@@ -1,17 +1,18 @@
 #pragma once
 
-
 // Common QMK settings
 #define TAP_CODE_DELAY 5
 #define TAP_HOLD_CAPS_DELAY 25
 #define GRAVE_ESC_SHIFT_OVERRIDE
 #define USB_SUSPEND_WAKEUP_DELAY 2000
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 1000U
+
 // Space saving options
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 #define LAYER_STATE_8BIT
 //#define NO_ACTION_ONESHOT
+
 // Tap-hold settings
 #define IGNORE_MOD_TAP_INTERRUPT
 #define TAPPING_TERM 220
@@ -21,6 +22,10 @@
 #define PERMISSIVE_HOLD_PER_KEY
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
 
+#ifdef COMBO_ENABLE
+#	define COMBO_TERM 30
+#	define COMBO_ONLY_FROM_LAYER 0
+#endif
 
 #ifdef SPLIT_KEYBOARD
 #	define SPLIT_WATCHDOG_ENABLE
@@ -41,8 +46,25 @@
 #	endif
 #endif
 
-#undef OLED_FONT_H
-#define OLED_FONT_H "oled/oledfont.c"
+#ifdef MOUSEKEY_ENABLE
+#	define MOUSEKEY_DELAY 0			// Delay between pressing a key and cursor movement
+#	define MOUSEKEY_INTERVAL 15		// Time between cursor movements in milliseconds
+#	define MOUSEKEY_MOVE_DELTA 10	// Step size for acceleration
+#	define MOUSEKEY_MAX_SPEED 9
+#	define MOUSEKEY_TIME_TO_MAX 90
+#	define MOUSEKEY_WHEEL_DELAY 16
+#	define MOUSEKEY_WHEEL_INTERVAL 30
+#	define MOUSEKEY_WHEEL_MAX_SPEED 10
+#	define MOUSEKEY_WHEEL_TIME_TO_MAX 95
+#endif
+
+#ifdef OLED_ENABLE
+#	define OLED_TIMEOUT 10000
+#	define OLED_BRIGHTNESS 180
+#	undef  OLED_FONT_H
+#	define OLED_FONT_H "oled/oledfont.c"
+#endif
+
 // Layout macros
 #ifndef __ASSEMBLER__
 #	include "layout.h"
