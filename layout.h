dc628f1f932e26db4723411f390af184839ba9d5
Ergotu <lambrechtsjordi@hotmail.be>
2023-03-19 14:13:51 +0100

    clean up unused defines

--- layout.h
+++ layout.h
@@ -11,10 +11,6 @@
 #define Z_SSAVE	HYPR(KC_S)
 
 // Tap hold macros
-#define TH_M	LT(0,KC_M)
-#define TH_COMM	LT(0,KC_COMM)
-#define TH_DOT	LT(0,KC_DOT)
-#define TH_SLSH	LT(0,KC_SLSH)
 #define TH(k1,k2,k3,k4) LT(0,k1),LT(0,k2),LT(0,k3),LT(0,k4)
 
 // Home row mod-tap macros
