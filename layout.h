90492208200629674e0fef4464a724b33981f9c8
Jordi Lambrechts <jordi@intigriti.com>
2024-09-03 12:38:58 +0200

    fix: move U defines to correct file

--- layout.h
+++ layout.h
@@ -1,5 +1,31 @@
 #include "quantum/keycodes.h"
 
+#if defined (CLIPBOARD_FUN)
+  #define U_RDO KC_AGIN
+  #define U_PST KC_PSTE
+  #define U_CPY KC_COPY
+  #define U_CUT KC_CUT
+  #define U_UND KC_UNDO
+#elif defined (CLIPBOARD_MAC)
+  #define U_RDO SCMD(KC_Z)
+  #define U_PST LCMD(KC_V)
+  #define U_CPY LCMD(KC_C)
+  #define U_CUT LCMD(KC_X)
+  #define U_UND LCMD(KC_Z)
+#elif defined (CLIPBOARD_WIN)
+  #define U_RDO C(KC_Y)
+  #define U_PST C(KC_V)
+  #define U_CPY C(KC_C)
+  #define U_CUT C(KC_X)
+  #define U_UND C(KC_Z)
+#else
+  #define U_RDO KC_AGIN
+  #define U_PST S(KC_INS)
+  #define U_CPY C(KC_INS)
+  #define U_CUT S(KC_DEL)
+  #define U_UND KC_UNDO
+#endif
+
 // macOS shortcuts
 #define Z_UND   G(KC_Z)
 #define Z_CUT   G(KC_X)
